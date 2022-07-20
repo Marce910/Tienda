1586fe6fb5348236b26466902135fcccd31e9833
Marce910 <109556649+Marce910@users.noreply.github.com>
2022-07-19 19:20:25 -0500

    Initial commit

---
+++ Codigo/Almacen.java
@@ -0,0 +1,47 @@
+import java.util.List;
+
+public class Almacen 
+{
+    private List<Producto> listaProductos;
+    
+    public Almacen() 
+    {
+        
+    }
+
+    public void agregarProducto(Producto p) 
+    {
+        
+    }
+
+    public void eliminarProducto(int codigo) 
+    {
+        
+    }
+
+    public void aumentarCantProducto(int codigo, int cant) 
+    {
+
+    }
+
+    public void disminuirCantProducto(int codigo, int cant) 
+    {
+   
+    }
+
+    public void modificarPrecio(int codigo, int precio) 
+    {
+
+    }
+
+    public Producto buscarProducto(String criterio) 
+    {
+        return null;
+    }
+
+    public Producto buscarProducto(int codigo) 
+    {
+        return null;
+    }
+
+}
