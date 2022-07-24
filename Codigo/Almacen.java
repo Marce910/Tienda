51e460c160450051f647ad8b8bdbb1d420489fcd
Marce910 <109556649+Marce910@users.noreply.github.com>
2022-07-24 00:35:49 -0500

    Modificadas clases y se incorporó la interacción

--- Codigo/Almacen.java
+++ Codigo/Almacen.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Almacen 
 {
@@ -6,42 +7,99 @@ public class Almacen
     
     public Almacen() 
     {
-        
+       this.listaProductos = new ArrayList<Producto>();
     }
-
+    
     public void agregarProducto(Producto p) 
     {
-        
+        this.listaProductos.add(p);
+    }
+    
+    public int buscarIndiceProducto(int codigo)
+    {
+        for (int i = 0; i < this.listaProductos.size(); i++)
+        {
+            if (this.listaProductos.get(i).getCodigo() == codigo)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Producto buscarProducto(int codigo)
+    {
+        for (Producto p: this.listaProductos)
+        {
+            if (p.getCodigo() == codigo)
+            {
+               return p; 
+            }
+        }
+        return null;
     }
 
     public void eliminarProducto(int codigo) 
     {
+        //Opcion1 cuando busco por codigo
+        int indiceABorrar = this.buscarIndiceProducto(codigo);
+        if (indiceABorrar != -1)
+        {
+            this.listaProductos.remove(indiceABorrar);
+        }
         
+        //Opcion2 cuando busco por producto
+        /*Producto productoABorrar = this.buscarProducto(codigo);
+        if (productoABorrar != null)
+        {
+            this.listaProductos.remove(productoABorrar);
+        }
+        */
     }
 
     public void aumentarCantProducto(int codigo, int cant) 
     {
-
+        int indiceAAumentarCantidad = this.buscarIndiceProducto(codigo);
+        if (indiceAAumentarCantidad != -1)
+        {
+            int nuevaCantidad = this.listaProductos.get(indiceAAumentarCantidad).getCantidad() + cant;
+            this.listaProductos.get(indiceAAumentarCantidad).setCantidad(nuevaCantidad);
+        }
+        
     }
 
     public void disminuirCantProducto(int codigo, int cant) 
     {
-   
+        int indiceAAumentarCantidad = this.buscarIndiceProducto(codigo);
+        if (indiceAAumentarCantidad != -1)
+        {
+            int nuevaCantidad = this.listaProductos.get(indiceAAumentarCantidad).getCantidad() - cant;
+            if (nuevaCantidad >=0)
+            {
+                this.listaProductos.get(indiceAAumentarCantidad).setCantidad(nuevaCantidad);
+            }
+        }
     }
 
-    public void modificarPrecio(int codigo, int precio) 
+    public void modificarPrecio(int codigo, int precio)
     {
-
+        int indiceAModificarPrecio = this.buscarIndiceProducto(codigo);
+        if (indiceAModificarPrecio != -1)
+        {
+            this.listaProductos.get(indiceAModificarPrecio).setPrecio(precio);
+        }
     }
 
-    public Producto buscarProducto(String criterio) 
+    public ArrayList<Producto> buscarProductos(String criterio) 
     {
-        return null;
+        ArrayList<Producto> listaResultado = new ArrayList<Producto>();
+        for (Producto p: this.listaProductos)
+        {
+            if (p.getNombre().equals(criterio) || p.getMarca().equals(criterio) || p.getPresentacion().equals(criterio) || p.getTipo().equals(criterio))
+            {
+                listaResultado.add(p);
+            }
+        }
+        return listaResultado;
     }
-
-    public Producto buscarProducto(int codigo) 
-    {
-        return null;
-    }
-
 }
